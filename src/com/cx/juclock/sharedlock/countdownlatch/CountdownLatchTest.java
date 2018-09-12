d53f0b5f407a887439727f7653816aaac40330b4
caoxiang <xiang.cao@teradata.com>
2018-09-12 22:38:44 +0800

    countdownlatch

--- src/com/cx/juclock/sharedlock/countdownlatch/CountdownLatchTest.java
+++ src/com/cx/juclock/sharedlock/countdownlatch/CountdownLatchTest.java
@@ -8,21 +8,21 @@ import java.util.concurrent.Executors;
 public class CountdownLatchTest {
     public static void main(String[] args) {
         ExecutorService service = Executors.newCachedThreadPool();
-        final CountDownLatch cdOrder = new CountDownLatch(1);
+        final CountDownLatch mainLatch = new CountDownLatch(1);
         final CountDownLatch cdAnswer = new CountDownLatch(3);
         for (int i = 0; i < 3; i++) {
             Runnable runnable = new Runnable() {
                 public void run() {
                     try {
-                        System.out.println("线程" + Thread.currentThread().getName() +
-                                "正准备接受命令");
-                        cdOrder.await();
-                        System.out.println("线程" + Thread.currentThread().getName() +
-                                "已接受命令");
-                        Thread.sleep((long) (Math.random() * 10000));
+                        System.out.println(Thread.currentThread().getName() +
+                                "running");
+                        mainLatch.await();
+                        System.out.println( Thread.currentThread().getName() +
+                                "running2");
+                        /*Thread.sleep((long) (Math.random() * 10000));
                         System.out.println("线程" + Thread.currentThread().getName() +
                                 "回应命令处理结果");
-                        cdAnswer.countDown();//3个线程分别减一
+                        cdAnswer.countDown();//3个线程分别减一*/
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -33,14 +33,15 @@ public class CountdownLatchTest {
 
         try {
             Thread.sleep((long) (Math.random() * 10000));
-            System.out.println("线程" + Thread.currentThread().getName() +
-                    "即将发布命令");
-            cdOrder.countDown();//计数器-1，初始值是1
-            System.out.println("线程" + Thread.currentThread().getName() +
+            System.out.println(Thread.currentThread().getName() +
+                    "running1");
+            mainLatch.countDown();
+
+            /*System.out.println("线程" + Thread.currentThread().getName() +
                     "已发送命令，正在等待结果");
             cdAnswer.await();//3个运动员都到了，裁判员公布成绩
             System.out.println("线程" + Thread.currentThread().getName() +
-                    "已收到所有响应结果");
+                    "已收到所有响应结果");*/
         } catch (Exception e) {
             e.printStackTrace();
         }
